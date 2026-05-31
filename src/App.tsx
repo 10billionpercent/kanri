72935f1a3e984ef139480a316fe17d6a5184b0e2
Shreya <shreya501711@gmail.com>
2026-05-31 22:22:48 +0530

    test VITE_API_URL

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { setUser } from "./reducers/userReducer";
 import { loadUser } from "./services/db";
 
 import Signup from "./pages/Signup";
-import ProjectPage from "./pages/ProjectPage";
+import ProjectPage from "./pages/ProjectPage"; //lol
 
 function App() {
   const user = useSelector((state: RootState) => state.user);
